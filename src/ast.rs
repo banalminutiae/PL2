c32e6d467f884581da7779415201103c529dfab4
andrew-vgw <andrew.li@vgw.co>
2025-10-23 13:34:37 +0800

    Implement fmt::Display for all AST nodes

--- src/ast.rs
+++ src/ast.rs
@@ -1,38 +1,64 @@
-use crate::token::{Token};
+use crate::token::Token;
+use std::fmt;
 
 pub trait Node {
-	fn token_literal(&self) -> &str;
+    fn token_literal(&self) -> &str;
 }
 
 #[derive(Debug)]
 pub enum Statement {
-	Let(LetStatement),
-	Return(ReturnStatement),
-	Expression(ExpressionStatement),
+    Let(LetStatement),
+    Return(ReturnStatement),
+    Expression(Expression),
 }
 
+// Statement ------------------------------------------------------------------------------
+
 impl Node for Statement {
-	fn token_literal(&self) -> &str {
+    fn token_literal(&self) -> &str {
+        match self {
+            Statement::Let(ls) => ls.token_literal(),
+            Statement::Return(rt) => rt.token_literal(),
+            Statement::Expression(exp) => exp.token_literal(),
+        }
+    }
+}
+
+impl fmt::Display for Statement {
+	fn fmt (&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
 		match self {
-			Statement::Let(ls) => ls.token_literal(),
-			Statement::Return(rt) => rt.token_literal(),
-			Statement::Expression(exp) => exp.token_literal(),
+			Statement::Let(lt) => write!(f, "{}", lt),
+			Statement::Return(rt) => write!(f, "{}", rt),
+			Statement::Expression(exp) => write!(f, "{}", exp),
 		}
 	}
 }
 
+// Expression -----------------------------------------------------------------------------
+
 #[derive(Debug)]
 pub enum Expression {
-	// Anything that resolves to a value. Bools, len(), string literals etc.
-	Identifier(Identifier),
-	IntegerLiteral(Identifier),
+    // Anything that resolves to a value. Bools, len(), string literals etc.
+    // Ifs and loops can go here because that'd be coo
+    // function calls, prefix and postifx operators, and comparisons are also expressions
+    Identifier(Identifier),
+    IntegerLiteral(Identifier),
 }
 
 impl Node for Expression {
-	fn token_literal(&self) -> &str {
+    fn token_literal(&self) -> &str {
+        match self {
+            Expression::Identifier(ident) => &ident.value,
+            Expression::IntegerLiteral(_) => "int",
+        }
+    }
+}
+
+impl fmt::Display for Expression {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
 		match self {
-			Expression::Identifier(ident) => &ident.value,
-			Expression::IntegerLiteral(_) => "int",
+			Expression::Identifier(ident) => write!(f, "{}", ident),
+			Expression::IntegerLiteral(il) => write!(f, "{}", il), 
 		}
 	}
 }
@@ -40,63 +66,81 @@ impl Node for Expression {
 #[derive(Debug)]
 // variable name of let x = 5, x is represented by the token.
 pub struct Identifier {
-	pub token: Token,
-	pub value: String,
+    pub token: Token,
+    pub value: String,
 }
 
-#[derive(Debug)]
-pub struct IntegerLiteral {
-	pub value: i64,
+impl fmt::Display for Identifier {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(f, "{}", self.value)
+	}
 }
 
 #[derive(Debug)]
 // Whole statement e.g. let x = 5;
 pub struct LetStatement {
-	pub token: Token,
-	pub name: Identifier,
-	// pub value: Expression,
+    pub token: Token,
+    pub name: Identifier,
+    pub value: Expression,
 }
 
 impl LetStatement {
-	pub fn token_literal(&self) -> &str {
-		"let"
-	}
-}	
+    pub fn token_literal(&self) -> &str {
+        "let"
+    }
+}
 
+impl fmt::Display for LetStatement {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(f, "let {} = {}", self.name, self.value)
+	}
+}
+	
 #[derive(Debug)]
 pub struct ReturnStatement {
-	pub token: Token,
-	// pub value: Expression,
+    pub token: Token,
+    pub value: Expression,
 }
 
 impl ReturnStatement {
-	pub fn token_literal(&self) -> &str {
-		"return"
+    pub fn token_literal(&self) -> &str {
+        "return"
+    }
+}
+
+impl fmt::Display for ReturnStatement {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(f, "return {}", self.value)
 	}
 }
 
 #[derive(Debug)]
-struct ExpressionStatement {
-	pub expression: Expression,
+pub struct IntegerLiteral {
+    pub value: i64,
 }
 
-impl ExpressionStatement {
-    pub fn token_literal(&self) -> &str {
-		self.expression.token_literal()
-	}
-}
+// Program -----------------------------------------------------------------------------
 
 #[derive(Debug)]
 pub struct Program {
-	pub statements: Vec<Statement>,
+    pub statements: Vec<Statement>,
 }
 
 impl Node for Program {
-	fn token_literal(&self) -> &str {
-		if self.statements.len() > 0 {
-			self.statements[0].token_literal()
-		} else {
-			""
-		}
-	}
+    fn token_literal(&self) -> &str {
+        if self.statements.len() > 0 {
+            self.statements[0].token_literal()
+        } else {
+            ""
+        }
+    }
+}
+
+impl fmt::Display for Program {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for stmt in &self.statements {
+            write!(f, "{}", stmt)?;
+        }
+        Ok(())
+    }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,164 +1,160 @@
+use crate::ast::{Identifier, LetStatement, Program, ReturnStatement, Statement};
+use crate::lexer::Lexer;
 use crate::token::{Token, TokenType};
-use crate::lexer::{Lexer};
-use crate::ast::{Program, Statement, LetStatement, ReturnStatement, Identifier};
 
 pub struct Parser<'a> {
-	lexer: Lexer<'a>,
-	curr_token: Token,
-	next_token: Token,
-	errors: Vec<String>,
+    lexer: Lexer<'a>,
+    curr_token: Token,
+    next_token: Token,
+    errors: Vec<String>,
 }
 
 impl<'a> Parser<'a> {
-	pub fn new(mut lexer: Lexer<'a>) -> Self {
-		let curr_token = lexer.next_token();
-		let next_token = lexer.next_token();
-		Self {
-			lexer,
-			curr_token,
-			next_token,
-			errors: Vec::new(),
-		}
-	}
+    pub fn new(mut lexer: Lexer<'a>) -> Self {
+        let curr_token = lexer.next_token();
+        let next_token = lexer.next_token();
+        Self {
+            lexer,
+            curr_token,
+            next_token,
+            errors: Vec::new(),
+        }
+    }
 
-	fn next_token(&mut self) {
-		self.curr_token = self.next_token.clone();
-		self.next_token = self.lexer.next_token();
-	}
+    fn next_token(&mut self) {
+        self.curr_token = self.next_token.clone();
+        self.next_token = self.lexer.next_token();
+    }
 
-	fn parse_program(&mut self) -> Program {
-		let mut program = Program { statements: Vec::new() };
-		while self.curr_token.token_type != TokenType::EOF {
-			if let Some(statement) = self.parse_statement() {
-				program.statements.push(statement);
-			}
-			self.next_token();
-		}
-		program
-	}
-	
-	fn parse_statement(&mut self) -> Option<Statement> {
-		match self.curr_token.token_type {
-			TokenType::LET => {
-				self.parse_let_statement().map(Statement::Let)
-			}
-			TokenType::RETURN => {
-				self.parse_return_statement().map(Statement::Return)
-			}
-			_ => None
-		}
-	}
+    fn parse_program(&mut self) -> Program {
+        let mut program = Program {
+            statements: Vec::new(),
+        };
+        while self.curr_token.token_type != TokenType::EOF {
+            if let Some(statement) = self.parse_statement() {
+                program.statements.push(statement);
+            }
+            self.next_token();
+        }
+        program
+    }
 
-	fn parse_let_statement(&mut self) -> Option<LetStatement> {
-		let current_token = self.curr_token.clone();
-		
-		if !self.expect_peek(TokenType::IDENTIFIER) {
-			return None;
-		}
+    fn parse_statement(&mut self) -> Option<Statement> {
+        match self.curr_token.token_type {
+            TokenType::LET => self.parse_let_statement().map(Statement::Let),
+            TokenType::RETURN => self.parse_return_statement().map(Statement::Return),
+            _ => None,
+        }
+    }
 
-		let statement = LetStatement {
-			token: current_token.clone(),
-			name: Identifier {
-				token: self.curr_token.clone(),
-				value: self.curr_token.literal.clone(),
-			},
-			// Expression here later
-		};
-		
-		if !self.expect_peek(TokenType::ASSIGN) {
-			return None;
-		}
+    fn parse_let_statement(&mut self) -> Option<LetStatement> {
+        let current_token = self.curr_token.clone();
 
-		// Temporarily skip expression parsing
-		while self.curr_token.token_type != TokenType::SEMICOLON {
-			self.next_token()
-		}
+        if !self.peek_and_consume_on_match(TokenType::IDENTIFIER) {
+            return None;
+        }
 
-		Some(statement)
-	}
+        let statement = LetStatement {
+            token: current_token.clone(),
+            name: Identifier {
+                token: self.curr_token.clone(),
+                value: self.curr_token.literal.clone(),
+            },
+            // Expression here later
+        };
 
-	fn parse_return_statement(&mut self) -> Option<ReturnStatement> {
-		let statement = ReturnStatement {
-			token: self.curr_token.clone(),
-		};
+        if !self.peek_and_consume_on_match(TokenType::ASSIGN) {
+            return None;
+        }
 
-		// Temporarily skip expression parsing
-		while self.curr_token.token_type != TokenType::SEMICOLON {
-			self.next_token()
-		}
+        // Temporarily skip expression parsing
+        while self.curr_token.token_type != TokenType::SEMICOLON {
+            self.next_token()
+        }
 
-		Some(statement)
-	}
+        Some(statement)
+    }
 
-	fn peek_error(&mut self, token_type: TokenType) {
-		let message = format!("Expected next token to be {:?}, got {:?} instead", token_type, self.next_token.token_type);
-		self.errors.push(message);
-	}
+    fn parse_return_statement(&mut self) -> Option<ReturnStatement> {
+        let statement = ReturnStatement {
+            token: self.curr_token.clone(),
+        };
 
-	fn expect_peek(&mut self, expected_type: TokenType) -> bool {
-		if self.next_token.token_type == expected_type {
-			self.next_token();
-			true
-		} else {
-			self.peek_error(expected_type);
-			false
-		}
-	}
+        // Temporarily skip expression parsing
+        while self.curr_token.token_type != TokenType::SEMICOLON {
+            self.next_token()
+        }
+
+        Some(statement)
+    }
+
+    fn peek_error(&mut self, token_type: TokenType) {
+        let message = format!(
+            "Expected next token to be {:?}, got {:?} instead",
+            token_type, self.next_token.token_type
+        );
+        self.errors.push(message);
+    }
+
+    fn peek_and_consume_on_match(&mut self, expected_type: TokenType) -> bool {
+        if self.next_token.token_type == expected_type {
+            self.next_token();
+            true
+        } else {
+            self.peek_error(expected_type);
+            false
+        }
+    }
 }
-
-
 
 #[cfg(test)]
 mod tests {
-	use super::*;
-	
-	#[test]
-	fn test_let_statement() {
-		let source = r#"
+    use super::*;
+
+    #[test]
+    fn test_let_statement() {
+        let source = r#"
            let x = 5;
            let y = 10;
 
            let foobar = 828282;
         "#;
 
-		let lexer = Lexer::new(source);
-		let mut parser = Parser::new(lexer);
+        let lexer = Lexer::new(source);
+        let mut parser = Parser::new(lexer);
 
-		let program = parser.parse_program();
-		assert_eq!(parser.errors.len(), 0);
-		assert_eq!(program.statements.len(), 3);
-		println!("{:#?}", program.statements);
-	}
+        let program = parser.parse_program();
+        assert_eq!(parser.errors.len(), 0);
+        assert_eq!(program.statements.len(), 3);
+        println!("{:#?}", program.statements);
+    }
 
-	#[test]
-	fn test_return_statement() {
-		let source = r#"
+    #[test]
+    fn test_return_statement() {
+        let source = r#"
             return 5;
         "#;
-		let lexer = Lexer::new(source);
-		let mut parser = Parser::new(lexer);
+        let lexer = Lexer::new(source);
+        let mut parser = Parser::new(lexer);
 
-		let program = parser.parse_program();
-		println!("{:?}", program.statements);
-		assert_eq!(parser.errors.len(), 0);
-		assert_eq!(program.statements.len(), 1);
-	}
+        let program = parser.parse_program();
+        println!("{:?}", program.statements);
+        assert_eq!(parser.errors.len(), 0);
+        assert_eq!(program.statements.len(), 1);
+    }
 
-	#[test]
-	fn test_parser_error() {
-		let source = r#"
+    #[test]
+    fn test_parser_error() {
+        let source = r#"
             let = 5;
             let 828282;
         "#;
 
-		let lexer = Lexer::new(source);
-		let mut parser = Parser::new(lexer);
+        let lexer = Lexer::new(source);
+        let mut parser = Parser::new(lexer);
 
-		let program = parser.parse_program();
-		assert_eq!(parser.errors.len(), 2);
-		println!("{:?}", parser.errors);
-	}
+        let _ = parser.parse_program();
+        assert_eq!(parser.errors.len(), 2);
+        println!("{:?}", parser.errors);
+    }
 }
-
-
-
